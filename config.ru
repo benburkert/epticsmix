5436496ddc9d6bccbccd6d7b4105b60136038f84
Ben Burkert <ben@benburkert.com>
2012-01-24 00:56:15 -0800

    Add the EpticsMix.setup! method.

--- config.ru
+++ config.ru
@@ -2,4 +2,6 @@ $:.unshift(File.join(File.dirname(__FILE__), 'lib'))
 
 require 'epticsmix'
 
+EpticsMix.setup!
+
 run EpticsMix::App.new
--- lib/epticsmix.rb
+++ lib/epticsmix.rb
@@ -1,8 +1,29 @@
 require 'sinatra/base'
 require 'sinatra_auth_github'
 require 'rack/client'
+require 'active_support'
+require 'active_record'
+require 'logger'
 
 module EpticsMix
+  def self.setup!
+    ActiveRecord::Base.establish_connection(database_config)
+
+    ActiveRecord::Base.logger = Logger.new STDOUT
+  end
+
+  def self.database_config
+    db = URI.parse(ENV['DATABASE_URL'])
+
+    {
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    }
+  end
 end
 
 require 'epticsmix/app'
