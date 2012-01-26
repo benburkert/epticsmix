9549a95397e46dcce20d9b5d8980ae63509b000b
Ben Burkert <ben@benburkert.com>
2012-01-26 15:42:57 -0800

    Derp a Derp.

--- lib/epticsmix/app.rb
+++ lib/epticsmix/app.rb
@@ -71,7 +71,7 @@ module EpticsMix
         users.inject(top_value) do |last_value, user|
           value      = user.send(method).to_i
           difference = last_value - value
-          m << "#{u.name.rjust(padding)}: #{value} ( #{difference.to_s.rjust(10)} to go)\n"
+          message << "#{user.name.rjust(padding)}: #{value} ( #{difference.to_s.rjust(10)} to go)\n"
 
           value
         end
