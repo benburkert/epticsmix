de1ef81119aa86bfc93734a5349bff1931d929f1
Ben Burkert <ben@benburkert.com>
2012-01-26 11:30:36 -0800

    Fix client & user.

--- lib/epticsmix/client.rb
+++ lib/epticsmix/client.rb
@@ -5,10 +5,6 @@ module EpticsMix
 
     attr_accessor :username, :password, :token
 
-    def self.login(username, password)
-      new(nil).login(username, password)
-    end
-
     def initialize(username, password)
       @username, @password = username, password
 
@@ -25,8 +21,41 @@ module EpticsMix
       token_from(response) if response.status == 200
     end
 
+    def season_stats
+      url = '/vailresorts/sites/epicmix/api/mobile/userstats.ashx'
+
+      response = get(url, headers, :timetype => 'season', :token => 'ABCDEFG1234567890')
+
+      return unless response.status == 200
+
+      if response['Content-Encoding'] == 'gzip'
+        body = Zlib::GzipReader.new(StringIO.new(response.body)).read
+      else
+        body = response.body
+      end
+
+      JSON.parse(body)['seasonStats']
+    end
+
     def token_from(response)
       response.headers['Set-Cookie'][%r{ASP.NET_SessionId=([^;]+);}, 1]
     end
+
+    def headers(extra = {})
+      extra.merge 'Cookie' => [session_id, website, session_id].join('; '),
+                  'Accept-Encoding' => 'gzip',
+                  'User-Agent' => 'EpicMix 15880 rv:2.1 (iPhone; iPhone OS 5.0.1; en_US)',
+                  'Host' => 'www.epicmix.com'
+
+    end
+
+    def session_id
+      "ASP.NET_SessionId=#{token}"
+    end
+
+    def website
+      "website#sc_wede=1"
+    end
+
   end
 end
--- lib/epticsmix/user.rb
+++ lib/epticsmix/user.rb
@@ -1,6 +1,21 @@
 module EpticsMix
   class User < ActiveRecord::Base
-    def feet
+
+    def client
+      @client ||= Client.new(self.username, self.password)
     end
+
+    def season_stats(year = 2011)
+      client.season_stats.detect {|season| season['year'] == year.to_i }
+    end
+
+    def vertical_feet
+      @vertical_feet ||= season_stats['verticalFeet']
+    end
+
+    def points
+      @points ||= season_stats['points']
+    end
+
   end
 end
