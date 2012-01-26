04181d588a8d30f353307fa3a1dd470f4c30f6a7
Ben Burkert <ben@benburkert.com>
2012-01-26 15:16:36 -0800

    Better login + vanity.

--- lib/epticsmix/app.rb
+++ lib/epticsmix/app.rb
@@ -17,6 +17,8 @@ module EpticsMix
       <<-HTML
   <html>
     <body>
+      <p>Hi there. I'm gonna need your username and password for http://www.epicmix.com/, it's probably your
+         email address. You did use a random password, right?</p>
       <form action='/login' method='post'>
         <p>epicmix.com email: <input type='text' name='username' /></p>
         <p>epicmix.com password: <input type='password' name='password' /></p>
@@ -66,12 +68,12 @@ module EpticsMix
         first_place = users.shift
         top_value   = first_place.send(method).to_i
 
-        message = "#{first_place.name.rjust(padding, ' ')}: #{top_value} #{type}\n"
+        message = "#{first_place.name.rjust(padding)}: #{top_value.to_s.rjust(10)} #{type}\n"
 
         users.inject(message) do |m, user|
           value      = user.send(method).to_i
           difference = top_value - value
-          m << "#{u.name.rjust(padding, ' ')}: #{value} ( #{difference} to go)\n"
+          m << "#{u.name.rjust(padding)}: #{value} ( #{difference.to_s.rjust(10)} to go)\n"
         end
 
         message
